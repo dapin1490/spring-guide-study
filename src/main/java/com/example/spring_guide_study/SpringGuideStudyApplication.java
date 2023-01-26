fa72f189e967c26ba14f3f6cccc40f62824220b2
dapin <dapin1490@gmail.com>
2023-01-26 23:57:11 +0900

    프로젝트명 변경

--- src/main/java/com/example/spring_guide_study/SpringGuideStudyApplication.java
+++ src/main/java/com/example/spring_guide_study/SpringGuideStudyApplication.java
@@ -1,13 +1,13 @@
-package com.example.spring_guide;
+package com.example.spring_guide_study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringGuideApplication {
+public class SpringGuideStudyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringGuideApplication.class, args);
+		SpringApplication.run(SpringGuideStudyApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/spring_guide_study/SpringGuideStudyApplicationTests.java
+++ src/test/java/com/example/spring_guide_study/SpringGuideStudyApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.spring_guide;
+package com.example.spring_guide_study;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringGuideApplicationTests {
+class SpringGuideStudyApplicationTests {
 
 	@Test
 	void contextLoads() {
