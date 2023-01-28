ae860523cd05982ede15d519fdc06a81f010cf79
dapin <dapin1490@gmail.com>
2023-01-28 21:37:35 +0900

    "Building REST services with Spring" 가이드 프로젝트 생성

---
+++ Building REST services with Spring/src/test/java/com/example/buildrestservice/BuildRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buildrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
