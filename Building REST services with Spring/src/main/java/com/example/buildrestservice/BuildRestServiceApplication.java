62c46e560c439d25ac2a844daf70427d634882b5
dapin <dapin1490@gmail.com>
2023-02-08 20:15:33 +0900

    "Building REST services with Spring" 가이드 진행중

--- Building REST services with Spring/src/main/java/com/example/buildrestservice/BuildRestServiceApplication.java
+++ Building REST services with Spring/src/main/java/com/example/buildrestservice/BuildRestServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BuildRestServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BuildRestServiceApplication.class, args);
 	}
-
 }
