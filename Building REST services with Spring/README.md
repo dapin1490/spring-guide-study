4715189ebdee6a6c73eb5cff67881ff8db1c77c3
dapin <dapin1490@gmail.com>
2023-02-10 00:32:29 +0900

    "Building REST services with Spring" 가이드 진행중

---
+++ Building REST services with Spring/README.md
@@ -0,0 +1,8 @@
+## cmd로 실행하기
+
+```shell
+$ cd C:\Users\dpgbu\Desktop\SAE\tool\github\spring-guide-study\Building REST services with Spring
+$ gradlew build
+$ java -jar build/libs/build-rest-service-0.0.1-SNAPSHOT.jar
+$ curl localhost:8080/employees
+```
--- Building REST services with Spring/src/main/java/com/example/buildrestservice/EmployeeNotFoundException.java
+++ Building REST services with Spring/src/main/java/com/example/buildrestservice/EmployeeNotFoundException.java
@@ -2,6 +2,6 @@ package com.example.buildrestservice;
 
 class EmployeeNotFoundException extends RuntimeException {
     EmployeeNotFoundException(Long id) {
-        super("Could not find employee " + id);
+        super("Could not find employee " + id + "\n");
     }
 }
